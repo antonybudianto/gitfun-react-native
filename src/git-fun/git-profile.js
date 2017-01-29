05364da4a293cdef8c6eb9147ff2f5295511ca9f
Antony Budianto <antonybudianto@gmail.com>
2017-01-29 20:51:35 +0700

    fetch user repo

--- src/git-fun/git-profile.js
+++ src/git-fun/git-profile.js
@@ -1,45 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     View,
+    ScrollView,
     Text,
     Image,
     TouchableHighlight
 } from 'react-native';
 
-const GitProfile = ({profile, back}) =>
-  <View style={{
-    flex: 1
-  }}>
-    <View style={{
-        flex: 0,
-        height: 70,
-        paddingTop: 20,
-        backgroundColor: 'orange',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
+class GitProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repos: []
+    };
+
+    this.fetchRepos();
+  }
+
+  fetchRepos() {
+    fetch(`https://api.github.com/users/${this.props.profile.login}/repos`)
+    .then(response => response.json())
+    .then(res => {
+      this.setState({
+        repos: res
+      })
+      console.log(res);
+    });
+  }
+
+  render() {
+    return (
+      <View style={{
+        flex: 1
       }}>
-      <TouchableHighlight underlayColor={'coral'} onPress={back}>
-        <Text style={{
-          padding: 15
-        }}>Back</Text>
-      </TouchableHighlight>
-    </View>
-    <View style={{
-      flex: 1,
-      padding: 10
-    }}>
-      <Image
-          style={{
-          width: 80,
-          height: 50,
-          }}
-          resizeMode={"contain"}
-          source={{uri: profile['avatar_url']}}
-      />
-      <Text>{profile.login}</Text>
-    </View>
-  </View>
+        <View style={{
+            flex: 0,
+            height: 70,
+            paddingTop: 20,
+            backgroundColor: 'orange',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
+          }}>
+          <TouchableHighlight underlayColor={'coral'} onPress={this.props.back}>
+            <Text style={{
+              padding: 15
+            }}>Back</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={{
+          flex: 1,
+          padding: 10
+        }}>
+          <Image
+              style={{
+              width: 80,
+              height: 50,
+              }}
+              resizeMode={"contain"}
+              source={{uri: this.props.profile['avatar_url']}}
+          />
+          <Text>{this.props.profile.login}</Text>
+          <Text>Total stars: &nbsp;
+            {this.state.repos
+              .filter(repo => !repo.fork)
+              .reduce((a,b) => a+b['stargazers_count'], 0)
+            }</Text>
+        </View>
+        <ScrollView style={{
+          flex: 1,
+          padding: 10
+        }}>
+          {
+            this.state.repos
+            .filter(repo => !repo.fork)
+            .map(repo =>
+              <View key={repo.id}>
+                <Text>{repo['name']} - {repo['stargazers_count']} stars</Text>
+              </View>
+            )
+          }
+        </ScrollView>
+
+      </View>
+    );
+  }
+}
+
 
 GitProfile.propTypes = {}
 GitProfile.defaultProps = {}
