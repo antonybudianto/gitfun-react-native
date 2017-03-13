cf13f0d9a9335f16040a8eaea43db54492022eb6
Antony Budianto <antonybudianto@gmail.com>
2017-03-14 06:52:20 +0700

    update to CRNA

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import GitFunApp from './src/app';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <GitFunApp></GitFunApp>
+    );
+  }
+}
--- index.android.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native'
-
-import App from './src/app';
-
-AppRegistry.registerComponent('todo', () => App);
