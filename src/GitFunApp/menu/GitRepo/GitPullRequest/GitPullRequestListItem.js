51f45ba9efdc007fa6ee86606374fb3d208bb73a
Antony Budianto <antonybudianto@gmail.com>
2017-04-02 17:16:48 +0700

    add login name

--- src/GitFunApp/menu/GitRepo/GitPullRequest/GitPullRequestListItem.js
+++ src/GitFunApp/menu/GitRepo/GitPullRequest/GitPullRequestListItem.js
@@ -16,46 +16,45 @@ const GitPullRequestListItem = ({pullRequest, onPress}) =>
     }}
     underlayColor="orange" onPress={onPress}>
     <View style={{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 5,
-        marginBottom: 10
-        }} key={pullRequest.id}>
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 5,
+      marginBottom: 10
+      }} key={pullRequest.id}>
+      <View style={{
+          flex:1,
+          flexDirection: 'row',
+          alignItems: 'flex-start'
+      }}>
+        <Image style={{
+            width: 30,
+            height: 30,
+            padding: 5
+          }}
+          resizeMode="contain"
+          source={{uri: pullRequest.user.avatar_url}} />
         <View style={{
-            flex:1,
-            flexDirection: 'row',
-            alignItems: 'flex-start'
+          flex: 1,
+          marginLeft: 10
         }}>
-            <Image style={{
-              width: 30,
-              height: 30,
-              padding: 5
-            }}
-            resizeMode="contain"
-            source={{uri: pullRequest.user.avatar_url}} />
-            <View style={{
-              flex: 1,
-              marginLeft: 10
-            }}>
-              <Text style={{fontWeight: 'bold'}}>#{pullRequest.number}: {pullRequest.title}</Text>
-              <Text style={{
-                fontSize: 10
-              }}>{pullRequest.base.repo.full_name}</Text>
-              <Text style={{
-                fontSize: 10
-              }}>State: {pullRequest.state}</Text>
-              <Text style={{
-                fontSize: 10
-              }}>{pullRequest.milestone && pullRequest.milestone.title}</Text>
-              <Text style={{
-                fontSize: 10,
-                marginTop: 5,
-                color: 'gray'
-              }}>{pullRequest.body}</Text>
-            </View>
-
+          <Text style={{fontWeight: 'bold'}}>#{pullRequest.number}: {pullRequest.title}</Text>
+          <Text style={{
+            fontSize: 10
+          }}>{pullRequest.base.repo.full_name}</Text>
+          <Text style={{
+            fontSize: 10
+          }}>By: {pullRequest.user.login}. State: {pullRequest.state}</Text>
+          <Text style={{
+            fontSize: 10
+          }}>{pullRequest.milestone && pullRequest.milestone.title}</Text>
+          <Text style={{
+            fontSize: 10,
+            marginTop: 5,
+            color: 'gray'
+          }}>{pullRequest.body}</Text>
         </View>
+      </View>
     </View>
   </TouchableHighlight>
 
